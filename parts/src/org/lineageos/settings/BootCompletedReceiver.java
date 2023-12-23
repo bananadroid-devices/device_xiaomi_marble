9439871a361e82ad0d37a9fb548b0ca03f9a668b
chaitanya <chaitanyaarora3@gmail.com>
2023-12-23 14:58:06 +0530

    marble: Disable Dirac

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -14,7 +14,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.camera.NfcCameraService;
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.display.ColorService;
 import org.lineageos.settings.dolby.DolbyUtils;
 import org.lineageos.settings.doze.AodBrightnessService;
@@ -53,12 +52,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         // AOD
         AodBrightnessService.startService(context);
 
-        // Dirac
-        try {
-            DiracUtils.getInstance(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
 
         // Refresh Rate
         RefreshUtils.initialize(context);
