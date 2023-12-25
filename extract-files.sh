e36691c8488e4de40bba0432067b10b54e5e4ad7
saikiran2001 <bjsaikiran@gmail.com>
2023-12-25 08:11:15 +0000

    marble: Fix crash of dolby decoders
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -172,6 +172,13 @@ function blob_fixup() {
     vendor/lib64/libdlbdsservice.so | vendor/lib64/soundfx/libhwdap.so)
         "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
         ;;
+    vendor/lib/libcodec2_hidl@1.0.stock.so)
+        "${PATCHELF}" --set-soname "libcodec2_hidl@1.0.stock.so" "${2}"
+        "${PATCHELF}" --replace-needed "libcodec2_vndk.so" "libcodec2_vndk.stock.so" "${2}"
+        ;;
+    vendor/lib/libcodec2_vndk.stock.so)
+        "${PATCHELF}" --set-soname "libcodec2_vndk.stock.so" "${2}"
+        ;;
     esac
 }
 
